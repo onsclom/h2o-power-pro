1ee71f6dc5b5786717d759b3217f353a49cd3b71
onsclom <onsclom@onsclom.net>
2023-09-30 15:58:28 -0700

    Tailwind start

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      serif:
+        "Seravek, 'Gill Sans Nova', Ubuntu, Calibri, 'DejaVu Sans', source-sans-pro, sans-serif",
+    },
+  },
+  plugins: [],
+}
