8ecb6c4ed75b3cedb1efd26242c56d0bd61c6c9a
onsclom <onsclom@onsclom.net>
2023-10-29 19:19:12 -0700

    Add link prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
 
-import tailwind from "@astrojs/tailwind";
+import prefetch from "@astrojs/prefetch"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [
+    tailwind(),
+    prefetch({
+      intentSelector: "a", // use intent for all anchor tags
+    }),
+  ],
+})
